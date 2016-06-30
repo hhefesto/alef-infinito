09cb1c4cdd1ab3ba58db459e4bf10d6e456a8d08
hefesto <daniel.herrera.rendon@gmail.com>
2016-06-30 01:58:14 -0500

    logBase incluído

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
+import Control.Concurrent
 
+circle :: Floating a => a -> a
+circle x
+  | (isInfinite x) = 0
+  | _ = circle (logBase 2 x)
 
-main :: Monoid Ordering a => a -> IO ()
-main mempty = putStrLn 0;
-main a = do
-  putStr 1
-  putStrLn a
-  a <> a
+main :: Floating a -> IO()
+main x = do
+  threadDelay 10000000
+  putStrLn (circle x)
